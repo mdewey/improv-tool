12f85d609df80f499f6b418cce9079a23178a32e
Mark Dewey <mark.t.dewey@gmail.com>
2018-07-11 05:55:52 -0400

    able to select Major scales

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const ALL_NOTES = []
-const LETTERS = ["A", "B", "C", "D", "E", "F", "G"]
-const MAJOR_SCALE_INTERVALS = [2, 2, 1, 2, 2, 2, 1];
-const PITCHES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+const LETTERS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G"
+]
+const MAJOR_SCALE_INTERVALS = [
+  2,
+  2,
+  1,
+  2,
+  2,
+  2,
+  1
+];
+const PITCHES = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B"
+]
 
-
-const cacluateScale = (startingNote) => {
+const calculateScale = (startingNote) => {
   let pointer = PITCHES.indexOf(startingNote);
-  console.log({ pointer, PITCHES, startingNote });
+  // console.log({pointer, PITCHES, startingNote});
   const notes = [];
   MAJOR_SCALE_INTERVALS.map((interval) => {
     notes.push(PITCHES[pointer]);
     pointer += interval
-    console.log({ pointer })
+    // console.log({pointer})
     if (pointer == 12) {
       pointer = 0;
     } else if (pointer === 13) {
@@ -23,48 +51,93 @@ const cacluateScale = (startingNote) => {
     }
   });
   notes.push(PITCHES[PITCHES.indexOf(startingNote)]);
-  console.log({ notes })
+  // console.log({notes})
   return notes;
 }
+
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      tonic: "C", 
-      scale:[]
+      tonic: "D",
+      scale: [],
+      currentlySelectedScale: "C",
+      selectedScale: [],
+      chordButtons: LETTERS
     }
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState((prev) => {
       return {
-        scale:cacluateScale( prev.tonic)
+        scale: calculateScale(prev.tonic),
+        selectedScale: calculateScale(prev.currentlySelectedScale)
       }
     })
   }
 
-
-
-
+  updateCurrentScale(event, newLetter) {
+    this.setState(() => {
+      return {selectedScale: calculateScale(newLetter), currentlySelectedScale: newLetter}
+    })
+  }
 
   render() {
     console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <h3>{this.state.tonic}</h3>
-        </p>
-        <ul>
-          {this.state.scale.map((note) => {
-            return <li>{note}</li>
-          })}
-          </ul>
+        <section>
+          build chord
+          <div>
+            <div>
+              {this
+                .state
+                .chordButtons
+                .map(letter => {
+                  return <button onClick={(e) => this.updateCurrentScale(e, letter)}>{letter}</button>
+                })}
+            </div>
+            <div>accents</div>
+            <div>Seventh</div>
+          </div>
+          <div>
+            <div>current chord</div>
+            <div>
+              <button>Add chord to song</button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <span>currently showing</span>
+              <span>{this.state.currentlySelectedScale}</span>
+
+            </div>
+            <div>
+              <ul>
+                {this
+                  .state
+                  .selectedScale
+                  .map((note, i) => {
+                    return <li key={i}>{note}</li>
+                  })}
+              </ul>
+            </div>
+          </div>
+        </section>
+        {/* <ul>
+          {this
+            .state
+            .selectedScale
+            .map((note) => {
+              return <li>{note}</li>
+            })}
+        </ul> */}
       </div>
     );
   }
